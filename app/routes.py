bf8db3613b37619ebf9ab5a40cc5ea80b4586ede
Shashi Kumar <ayushmankasyap@gmail.com>
2025-12-16 21:15:59 +0530

    Update API key in routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Todo
 
 routes = Blueprint('routes', __name__)
-apikeY='sk137sgw9shwheoooosg27u&#767373ss'
+apikeY='sk137sgw9shwheoooosg27u&#767373ssjh'
 @routes.route('/')
 def index():
     todos = Todo.get_all()
